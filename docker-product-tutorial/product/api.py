40d37dea469a15aa33daa3f2abb1b266a140f85c
Tushar Iyer <tushariyer@users.noreply.github.com>
2018-08-24 20:54:58 -0400

    Uploaded tutorials

---
+++ docker-product-tutorial/product/api.py
@@ -0,0 +1,21 @@
+# Docker Tutorial Python API
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Product(Resource):
+	def get(self):
+		return {
+			'products': ['Pizza',
+						 'Pasta',
+						 'Parmeggianno',
+						 'Chocolate',
+						 'Ice Cream']
+		}
+api.add_resource(Product, '/')
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=80, debug=True)
